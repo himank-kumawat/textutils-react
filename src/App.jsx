975af8883b4168b970bc456a3a469c1de0143fe4
himank <himank.kumawat1998@gmail.com>
2025-11-17 23:22:30 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+import Alert from "./components/Alert";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({ msg: message, type: type });
+    setTimeout(() => setAlert(null), 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode enabled", "success");
+    }
+  };
+
+  return (
+    <>
+      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <Routes>
+          <Route path="/about" element={<About mode={mode} />} />
+          <Route
+            path="/"
+            element={
+              <TextForm
+                heading="Try TextUtils - Word Counter, Character Counter"
+                showAlert={showAlert}
+                mode={mode}
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
